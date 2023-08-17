117b2b0ec7388d8dc8963d6b0ac7cf9d733ebdf4
Pikosha2101 <Puka4yBTanke@gmail.com>
2023-08-17 22:26:56 +0300

    registration

---
+++ app/src/main/java/com/example/laba4_4/DBHelper.kt
@@ -0,0 +1,33 @@
+package com.example.laba4_4
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+class DBHelper(
+    context: Context) :
+    SQLiteOpenHelper(context, name, null, version) {
+    companion object{
+        private const val name = "dblab4"
+        private const val version = 1
+    }
+    override fun onCreate(db: SQLiteDatabase?) {
+        Log.d("myLogs", "---On create database---")
+        db?.execSQL("create table mytable ("
+                + "id integer primary key autoincrement,"
+                + "login text,"
+                + "password text,"
+                + "count integer" + ");")
+    }
+
+
+    private var TABLE_CONTACTS = "myDB"
+
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("drop table if exists $TABLE_CONTACTS")
+
+        onCreate(db)
+    }
+}
